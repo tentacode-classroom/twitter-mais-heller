40fa8095384ee76cb8c96beadaae8ff03b7d9d5a
Florian <florian.a@live.fr>
2018-10-12 17:57:21 +0200

    Debut commande app:install

---
+++ src/Command/CommandInstall.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\ArrayInput;
+
+class CommandInstall extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('app:install')
+            ->setDescription('Install the project')
+            ->setHelp('This command allows you to install the project')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln([
+            'Project Heller Installer',
+            '============',
+            '',
+        ]);
+
+        
+ 
+        $output->writeln('Installing the project ... ');
+
+        $commands = [
+            'composer require orm-fixtures --dev',
+        ];
+
+    foreach($commands as $command){
+        run($command);
+    }
+        
+    
+    }
+}
