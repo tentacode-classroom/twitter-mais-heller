2b4ec8f13207f2641462b107b7f78f6ef580b878
Florian <florian.a@live.fr>
2018-10-15 11:46:47 +0200

    Form message

--- src/Controller/UserFeedController.php
+++ src/Controller/UserFeedController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use App\Entity\Message;
+use App\Form\MessageType;
 
 class UserFeedController extends AbstractController
 {
@@ -17,9 +19,31 @@ class UserFeedController extends AbstractController
         $user = $this->getDoctrine()
         ->getRepository(User::class)
         ->find($userId);
+
+        $message = new Message();
+
+
+        $formMessage = $this->createForm(MessageType::class, $message);
+        $formMessage->handleRequest($request);
+
+        if ($formMessage->isSubmitted() && $formMessage->isValid()) {
+            $message = $formMessage->getData();
+
+            $message->setPostDate(new \DateTime('NOW'));
+            $message->setUser($user);
+            $message->setLikes(0);
+            $message->setRetweet(0);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            
+            return $this->redirect($request->getUri());
+        }
    
     return $this->render('UserFeed/index.html.twig', [
         'user' => $user,
+        'formMessage' => $formMessage->createView(),
         ]);
     }
 }
